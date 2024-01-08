88174945ef9fa11bc1ba1c760e2bb9bd402e7f66
mrlaboratory <mrlaboratory23@gmail.com>
2024-01-09 02:25:56 +0600

    added besic file

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../shared/Navbar';
+
+const Admin = () => {
+    return (
+        <div>
+    
+
+        </div>
+    );
+};
+
+export default Admin;
